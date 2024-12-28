9ba8eaf7c50539b1c7eb1cab0dc34276b1d1f418
Ayush tomar <ayushtomar388@gmail.com>
2024-12-28 23:53:35 +0530

    Server component created

--- src/components/servers/ServerHeader.tsx
+++ src/components/servers/ServerHeader.tsx
@@ -55,7 +55,10 @@ const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 { isModerator && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={() =>onOpen("createChannel")}
+                        className="px-3 py-2 text-sm cursor-pointer"
+                    >
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto"/> 
                     </DropdownMenuItem>
@@ -64,13 +67,17 @@ const ServerHeader = ({
                     <DropdownMenuSeparator/>
                 )}
                 { isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={() => onOpen("deleteServer", {server})}
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto"/> 
                     </DropdownMenuItem>
                 )}
                 { !isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={() => onOpen("leaveServer", {server})}
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/> 
                     </DropdownMenuItem>
