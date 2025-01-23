9465d4e503b3b4fffe1d8b6307798a3fcdc8616e
Ayush tomar <ayushtomar388@gmail.com>
2025-01-23 23:43:21 +0530

    Del : Extra files deleted

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({children} : {children : React.ReactNode}) => {
-    return ( 
-        <div className="flex flex-col items-center justify-center h-full">
-            {children}
-        </div>
-     );
-}
- 
-export default AuthLayout;
